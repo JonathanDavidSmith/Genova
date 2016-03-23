280871dfb12242b89596b45ccad9b5cb413cea2f
Jonathan Smith <jonathan.smith.prosper@gmail.com>
2016-03-22 18:46:23 -0600

    added some more comments to help explain coding thought process

--- src/controllers/Controllers.java
+++ src/controllers/Controllers.java
@@ -10,43 +10,40 @@ import org.springframework.web.servlet.ModelAndView;
 import nameAndAge.FileData;
 
 @Controller
-public class Controllers 
-{
+public class Controllers {
 	@Autowired
 	private FileData dao;
 	
-	@RequestMapping(path="names.do", method = RequestMethod.GET)
-	public ModelAndView getPerson()
-	{
+	
+	//This method simply displays the text files combined into one list.
+	@RequestMapping(path = "names.do", method = RequestMethod.GET)
+	public ModelAndView getPerson() {
 		ModelAndView mv = new ModelAndView("display.jsp");
 		mv.addObject("Names", dao.getPeople());
 		return mv;
 	}
-	@RequestMapping(path="averageAge.do", method = RequestMethod.GET)
-	public ModelAndView calculateAverageAge()
-	{
+	//This method sends the average age of all the people.
+	@RequestMapping(path = "averageAge.do", method = RequestMethod.GET)
+	public ModelAndView calculateAverageAge() {
 		ModelAndView mv = new ModelAndView("average.jsp");
 		mv.addObject("Average", dao.calculateAverageAge());
 		return mv;
 	}
-	@RequestMapping(path="alphabetical.do", method = RequestMethod.GET)
-	public ModelAndView alphabetical()
-	{
+	//This method sends the alphabetized list via last name to be displayed.
+	@RequestMapping(path = "alphabetical.do", method = RequestMethod.GET)
+	public ModelAndView alphabetical() {
 		ModelAndView mv = new ModelAndView("alphabetical.jsp");
 		mv.addObject("alphabetical", dao.alphabetical());
 		return mv;
 	}
-//	When implemented, This will allow the number of duplicated first names to be seen.  
-//	Utilizing the duplicates() method in FileData.java.
-//	The method has not yet been completed, hence the commented out code. 
-	
-	@RequestMapping(path="duplicates.do", method = RequestMethod.GET)
-	public ModelAndView duplicates()
-	{
+
+	// This method will allow the number of duplicated first names to be seen.
+	// Utilizing the duplicates() method in FileData.java.
+	@RequestMapping(path = "duplicates.do", method = RequestMethod.GET)
+	public ModelAndView duplicates() {
 		ModelAndView mv = new ModelAndView("duplicates.jsp");
 		mv.addObject("duplicates", dao.duplicates());
 		return mv;
 	}
-	
-	
+
 }
--- src/nameAndAge/FileData.java
+++ src/nameAndAge/FileData.java
@@ -18,14 +18,15 @@ import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 
-
 public class FileData {
-	//here we are creating private final variables that cannot be changed from outside classes. 
-	//we will be using these variables for importing the two text files and instantiating a new arraylist for the data to be stored.
+	// here we are creating private final variables that cannot be changed from
+	// outside classes.
+	// we will be using these variables for importing the two text files and
+	// instantiating a new arraylist for the data to be stored.
 	private static final String names1 = "/WEB-INF/NameList1.txt";
 	private static final String names2 = "/WEB-INF/NameList2.txt";
 	private List<Person> people = new ArrayList<>();
-	
+
 	@Autowired
 	private ApplicationContext ac;
 
@@ -80,66 +81,45 @@ public class FileData {
 		System.out.println(people);
 
 	}
-	//this method calculates the average age utilizing a foreach loop through the array of people.
-	public double calculateAverageAge() 
-	{
+
+	// this method calculates the average age utilizing a foreach loop through
+	// the array of people.
+	public double calculateAverageAge() {
 		double sum = 0d;
 		double average = 0d;
-		for (Person person : people) 
-		{
-			sum+= person.getAge();
+		for (Person person : people) {
+			sum += person.getAge();
 		}
-		average = sum/(people.size());
+		average = sum / (people.size());
 		System.out.println(average);
 		return average;
-		}
-	//this method puts the last names in alphabetical order
-	public List<Person> alphabetical()
-	{	
+	}
+
+	// this method puts the last names in alphabetical order
+	public List<Person> alphabetical() {
 		Collections.sort(people);
 		return people;
 	}
-//	This (NOT YET COMPLETE..DOES NOT WORK) method counts the number of first names that are duplicated.
-	
-//	public int getDupCount(ArrayList<Person> people)
-//	{
-//		int count = 0;
-//		Map<String, Integer> unique = new HashMap<>(people);
-//		
-//		for (Person p : people)
-//		{
-//			if (Collections.frequency(people, p) > people)
-//				count++;
-//		}
-//	}
-//	
-	
-	//this method below prints duplicated names to the console successfully.
-	//commenting it out because I'm working on a different method that will
-	//produce similar results in a return statement.
-	public String duplicates() 
-	{ 	
+
+	// this method below prints duplicated first names.
+	public String duplicates() {
 		Map<String, Integer> unique = new HashMap<>(1);
 		int i = 0;
-		for (Person p : people)
-		{
-			if(unique.containsKey(p.getFname()))
-			{	
+		for (Person p : people) {
+			if (unique.containsKey(p.getFname())) {
 				System.out.println(p.getFname());
-				 i = unique.get(p.getFname());
-						unique.put(p.getFname(), i + 1);
+				i = unique.get(p.getFname());
+				unique.put(p.getFname(), i + 1);
 				i++;
-			}
-			else
-			{
+			} else {
 				unique.put(p.getFname(), 1);
 			}
 		}
 		return i + "";
-		
+
 	}
-        
-	//	this method returns the entire arraylist of "people".
+
+	// this method returns the entire arraylist of "people".
 	public List<Person> getPeople() {
 		return people;
 	}
